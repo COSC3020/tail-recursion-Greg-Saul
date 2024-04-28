2cc5a501846380346e668a6a475397eafa2c715b
Greg-Saul <123204473+Greg-Saul@users.noreply.github.com>
2024-04-28 10:38:53 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,1 +1,24 @@
+function tailFib(n){
+    if(n < 1){
+        return 0;
+    }
 
+    return tail(n, 0, 1)
+}
+
+function tail(n, minusTwo, minusOne){
+    if(n == 1){
+        return minusOne;
+    }
+    let arr = addition(minusTwo, minusOne)
+    return tail(n - 1, arr[0], arr[1])
+}
+
+function addition(two ,one){
+    let tmp = one;
+    one += two;
+    two = tmp;
+    return [two, one]
+}
+
+module.exports = {tailFib};
