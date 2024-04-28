c3e2643ef69bc53ed436f78a2d221904e3a58d58
Greg-Saul <123204473+Greg-Saul@users.noreply.github.com>
2024-04-28 11:34:26 -0600

    Update code.js

--- code.js
+++ code.js
@@ -10,16 +10,16 @@ function tail(n, minusTwo, minusOne){
     if(n == 1){
         return minusOne;
     }
-    let arr = addition(minusTwo, minusOne)
-    return tail(n - 1, arr[0], arr[1])
+    // let arr = addition(minusTwo, minusOne)
+    return tail(n - 1, minusOne, minusTwo + minusOne)
 }
 
-function addition(two ,one){
-    let tmp = one;
-    one += two;
-    two = tmp;
-    return [two, one]
-}
+// function addition(two ,one){
+//     let tmp = one;
+//     one += two;
+//     two = tmp;
+//     return [two, one]
+// }
 
 module.exports = {tailFib};
 
